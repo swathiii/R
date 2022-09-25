1b7bb9ff66fb5d5b9dd197e0131e2c21801d4289
Swathi <35723630+swathiii@users.noreply.github.com>
2022-09-25 21:23:52 +0100

    Add files via upload

---
+++ DataTypes.R
@@ -0,0 +1,31 @@
+# <- is an assignment operator 
+
+#------------------DATA TYPES---------------------
+
+x <- 2L 
+#R by default stores the integer variable as double
+#hence by mentioning 'L' we specify 2 is an integer 
+
+#check data-type
+typeof(x)
+
+#double 
+y <- 2.5
+typeof(y)
+
+#complex 
+z <- 3 + 2i 
+typeof(z)
+
+#character 
+a <- "h"  #use quotation to insert letters 
+typeof(a)
+
+#Logical 
+q1 <- T
+typeof(q1)
+
+q2 <- FALSE
+typeof(q2)
+
+
