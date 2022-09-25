7f5dce015c94a97df00d8266ec50d78fd6062f5a
Swathi <35723630+swathiii@users.noreply.github.com>
2022-09-25 21:51:25 +0100

    Add files via upload

---
+++ WhileLoop.R
@@ -0,0 +1,18 @@
+#------------WHILE LOOP----------------------------------
+
+while(FALSE){
+  print("Hello") #to print something within a loop we need to use the print func
+}
+
+while(TRUE){
+  print("Hello") 
+}
+#Will execute an infinite loop, press ESC to exit
+
+
+counter <- 1
+
+while(counter < 12 ){
+  print(counter)
+  counter <- counter + 1 
+}
