5550b2a6c6a5ea5f02ac0cb00e66962b9b5b4fc8
Swathi <35723630+swathiii@users.noreply.github.com>
2022-09-25 21:33:24 +0100

    Add files via upload

---
+++ operations.R
@@ -0,0 +1,27 @@
+#---------operations------------------------
+
+A <- 10 
+B <- 5
+
+C <- A + B #spaces in R don't matter 
+
+C #call object to print it, no special function
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1/var2   #performing an operation 
+result
+
+answer <- sqrt(var2)  #using a function 
+answer 
+
+#------------Character operations--------------------
+
+greeting <- "Hello"
+name <- "Bob!"
+
+message <- paste(greeting, name)
+message 
+
+#paste() function is used to string characters together 
