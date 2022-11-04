3751b835c9b8ce640385f19c65a28b149debf074
Swathi <35723630+swathiii@users.noreply.github.com>
2022-11-04 14:11:24 +0000

    Add files via upload

--- IDS_week5.R
+++ IDS_week5.R
@@ -152,12 +152,12 @@ ggplot(
   hills_resid, aes(climb, time)
 ) + 
   geom_point(size=3) +
-  geom_point(size=2, aes(y=predicted), shape=1)+ #show the predicted values
-  geom_segment(aes(xend=climb, yend=predicted), alpha = 0.9, color='purple') +
+  geom_point(size=2, aes(y=predicted), shape=2)+ #show the predicted values
+  geom_segment(aes(xend=climb, yend=predicted), alpha = 0.9, color='red') +
   geom_abline(mapping = aes(
     slope=coef_climb["climb"],
     intercept=coef_climb["(Intercept)"]
-  ), color='red'
+  ), color='blue'
 )
 
 
@@ -168,5 +168,312 @@ plot(
 
 #the outliers are different : Knock hill, Lairig Ghru and Creag Dubh ------???------there is not that much of a variance for knock hill, why is bens of Jura not mentioned?
 
-
+?geom_point
 #----Making predictions with the linear regression model---#
+
+#we can use R's predict function and we can pass new unseen data to it
+predict(
+  mod_dist,
+  newdata = hills_test
+)
+
+#include CI of 95% to get a sense of how reliable these predictions are
+predict(
+  mod_dist,
+  newdata = hills_test,
+  interval = 'confidence'
+)
+#gives us a range of possible values around the estimates with swing of 8-20 minutes estimate either way 
+
+#calculating the residuals now that we have true values, diff between predictions and true values
+hills_dist_test <- hills_test #making a copy of the test data 
+hills_dist_test$predicted <- predict(mod_dist, newdata=hills_dist_test)
+#calculating the residual
+hills_dist_test$residuals <- hills_dist_test$predicted - hills_dist_test$time
+
+hills_dist_test
+
+#the predictions are all just about wihtin the 95% confidence interval, so the model fit is pretty strong
+#we can graph the residuals to see it in a different way 
+
+ggplot(
+  data=hills_dist_test,
+  aes(x=dist, y=time)
+) +
+  geom_point(size=3) +   #make the actual values show up clearly
+  geom_point(size=2, aes(y=predicted), shape=2) + #showing the predicted values
+  geom_segment(aes(xend=dist, yend=predicted), alpha = 0.9, color='red') + #plotting a line from the predicted value to true value 
+  geom_abline(
+    mapping=aes(
+      slope=coef_dist['dist'],
+      intercept=coef_dist['(Intercept)']
+    ), color= 'blue' 
+    )
+
+#calculating the sum of squared errors to get a single overall number we can use for
+#comparing different models in terms of how well they predict the test data
+sse_dist <- sum(hills_dist_test$residuals**2)
+sse_dist
+
+#EXERCISE : use the climb model to make predictions : how do they compare? are the 
+#errors different? what does it tell you about their fit?
+mod_climb
+hills_climb_test
+coef_climb
+
+hills_climb_test <- hills_test
+hills_climb_test$predicted <- predict(mod_climb, newdata = hills_climb_test)
+hills_climb_test$residuals <- hills_climb_test$predicted - hills_climb_test$time
+
+ggplot(data=hills_climb_test,
+       aes(x= climb, y=time)
+       ) +
+  geom_point(size=3) +
+  geom_point(size = 2, aes(y=predicted), shape = 2) +
+  geom_segment(aes(xend=climb, yend=predicted), alpha=0.9, color='red') +
+  geom_abline(mapping=aes(
+    slope= coef_climb['climb'],
+    intercept = coef_climb['(Intercept)']
+  ), color='blue')
+
+sse_climb <- sum(hills_climb_test$residuals**2)
+sse_climb
+
+#---multi-variate linear regression---#
+
+#a regression with two or more variables
+#colinearity : when two variables change in line with each other
+
+#checking dist and climb together(ignoring time for now) to see if they look colinear
+ggplot(
+  data=hills_train,
+  aes(x=dist, y=climb)
+) + geom_point()
+
+#there is some relation but a lot of scatter, which means adding them together can be helpful
+cor.test(hills_train$dist, hills_train$climb)
+#correlation of 0.53 says they're definitely correlated but they're also definitely not perfect 
+#predictors of each other
+
+#plotting dist and climb together with time to see how they relate : using rgl library 
+library(rgl)
+plot3d(
+  x=hills_train$dist,
+  y=hills_train$climb,
+  z=hills_train$time
+)
+rglwidget()
+
+#training the multivariate regression, we use lm() command but now indicate multiple predictor variables
+mod_hills <- lm(
+  formula = time~climb+dist,
+  data=hills_train
+)
+
+#we can still visualize the regression model but now in a 2d plane instead of a line
+open3d()
+
+plot3d(
+  x=hills_train$dist,
+  y=hills_train$climb,
+  z=hills_train$time,
+  type='s', size=2, col='purple' #show the data points as purple spheres for visibility
+)
+
+coefs <- coef(mod_hills)
+planes3d(a=coefs["dist"],
+         b=coefs["climb"],
+         c= -1,
+         d=coefs["(Intercept)"], col='grey')
+rglwidget()
+?planes3d()
+
+#EXERCISE : using predict() method calculate residuals and SSE
+#compare it with the single-variable model
+
+plot(
+  mod_hills,
+  which=1
+)
+
+#OR to plot all three side by side
+layout(matrix(1:3, ncol=3))
+plot(mod_dist, which=1) # dist-based model
+plot(mod_climb, which=1) # climb-based model
+plot(mod_hills, which=1) 
+
+predict(
+  mod_hills,
+  newdata=hills_test,
+  interval='confidence'
+)
+
+hills_final_test <- hills_test
+hills_final_test$predicted <- predict(mod_hills, newdata=hills_final_test)
+hills_final_test$residuals <- hills_final_test$predicted - hills_final_test$time
+
+hills_final_test
+
+#calculating the SSE
+sse_hills_final <- sum(hills_final_test$residuals**2)
+sse_hills_final #608.8576
+
+#other factors not included
+#in the dataset: weather, fitness, breaks
+
+
+#---Classification and linear separability---#
+
+#setting up the iris dataset
+data("iris")
+iris[1:5,]
+
+#to make sure that our training data and test data includes
+#samples of all three species, we'll first shuffle the rows
+
+iris <- iris[sample(1:nrow(iris)),] #shuffling the rows of the dataframe in random order
+iris[1:5,]
+
+#using 70% of the data for training and 30% for testing [70/30 split]
+#separate randomly into training and test data
+train_size = 0.7  #using 70% of data for training
+iris_train <- iris[1:(train_size*nrow(iris)),]
+iris_test <- iris[(nrow(iris_train)+1):nrow(iris),] #--------???-------explanation
+
+
+#Visualizing data for classification
+#with classification the goal is to draw a line that SEPARATES the categories of the data
+
+#to graph the iris dataset using 2 of the 4 independent features : sepal length/width
+
+colors <- c('#1b9e77', '#d95f02', '#7570b3') # colors chosen to be visually una
+#mbiguous: see https://colorbrewer2.org/
+
+iris_train_colors <- colors[as.numeric(iris_train$Species)]
+shapes <- c('o', '+', 'x')
+iris_train_shapes <- shapes[as.numeric(iris_train$Species)]
+
+ggplot(
+  data=iris_train,
+  aes(x=Sepal.Length, y=Sepal.Width)
+) +
+  geom_point(color=iris_train_colors, shape=iris_train_shapes, size=5)
+
+#Linear Separability
+
+#for now, we're just going to look at whether the Specie is viginica or not
+
+#-------------??????------------------
+binaryColors <- function(data, species) {
+  tf_values <- data$Species == species
+  color_indices <- as.numeric(tf_values)+1
+  return(colors[color_indices])
+}
+
+binaryShapes <- function(data, species) {
+  tf_values <- data$Species == species
+  shape_indices <- as.numeric(tf_values)+1
+  return(shapes[shape_indices])
+}
+
+#looking at sepal l/w
+binarySpecies = 'virginica'
+
+ggplot(
+  data = iris_train,
+  aes(x=Sepal.Length, y=Sepal.Width)
+) +
+  geom_point(
+    color = binaryColors(iris_train, binarySpecies),
+    shape = binaryShapes(iris_train, binarySpecies),
+    size=5
+  )
+
+#looking at petal l/w
+binarySpecies = 'virginica'
+ggplot(
+  data= iris_train,
+  aes(x=Petal.Length, y=Petal.Width)
+) +
+  geom_point(
+    color = binaryColors(iris_train, binarySpecies),
+    shape = binaryShapes(iris_train, binarySpecies),
+    size = 5
+  )
+
+#we note that when using Petal.Length and Petal.Width independent variables,
+#we can separate the classes in the data pretty well, these data are (mostly) linearly separable
+
+#EXERCISE : graph the binary class labels for the other two species : setosa and versicolor
+#how seperable are these classes? How well would a LM work for classifying these bunary versions of the data
+
+#SETOSA
+binarySpecies = 'setosa'
+ggplot(
+  data=iris_train,
+        aes(x=Sepal.Length, y=Sepal.Width)
+        ) +
+    geom_point(
+      color = binaryColors(iris_train, binarySpecies),
+      shape = binaryShapes(iris_train, binarySpecies),
+      size = 5
+    )
+
+binarySpecies = 'setosa'
+ggplot(
+  data=iris_train,
+  aes(x=Petal.Length, y=Petal.Width)
+) +
+  geom_point(
+    color = binaryColors(iris_train, binarySpecies),
+    shape = binaryShapes(iris_train, binarySpecies),
+    size = 5
+  )
+
+#VERSICOLOR
+binarySpecies = 'versicolor'
+ggplot(
+  data = iris_train, 
+  aes(x=Sepal.Length, y=Sepal.Width)
+) +
+  geom_point(
+    color = binaryColors(iris_train, binarySpecies),
+    shape = binaryShapes(iris_train, binarySpecies),
+    size = 5
+  )
+
+binarySpecies = 'versicolor'
+ggplot(
+  data=iris_train,
+  aes(x=Petal.Length, y=Petal.Width)
+) +
+  geom_point(
+    color = binaryColors(iris_train, binarySpecies),
+    shape = binaryShapes(iris_train, binarySpecies),
+    size = 5
+  )
+
+#----LOGISTING REGRESSION - BINOMIAL----#
+#predicting a binary(yes/no) variable.
+
+#--Learning the logistic regression model--#
+#transforming the iris data to have a binary label for prediction. We'll start by predicting if a given sample is virginica or not 
+
+iris_train$binarySpecies <- iris_train$Species == 'virginica'
+iris_train$binarySpecies <- iris_train$binarySpecies * 1 #convert from TRUE/FALSE to 1/0
+
+iris_train[1:10,] #examining data to verify is binaryspecies label was set correctly
+
+#R's glm() function to estimate a logistic regression model based on training data
+#it has the same syntax as lm(), also need to specify family argument for the function
+
+?glm()
+
+#start by using petal length/width to predict the class
+iris_binary_model <- glm(
+  binarySpecies ~ Petal.Width + Petal.Length, #predicting the binarySpecies label usig petal l/w
+  family = binomial(link = 'logit'), #use a logistic regression
+  data = iris_train
+)
+
+#officially trained my first machine learning model !!
