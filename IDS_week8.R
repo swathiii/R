afa575379eee7dc95a07f410c0ce065f3897932d
Swathi <35723630+swathiii@users.noreply.github.com>
2022-11-22 11:57:51 +0000

    Add files via upload

---
+++ IDS_week8.R
@@ -0,0 +1,11 @@
+#WEEK 8 - DATA VISUALIZATION WITH R
+
+#-----------------------EXPLORING THE TIME-SERIES DATA--------------------
+
+library(tidyverse)
+library(lubridate)
+
+#nycfligts13 contains example data that we can use for a time-series graph
+library(nycflights13)
+flights
+View(flights)
